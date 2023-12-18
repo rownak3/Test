576da936dc4b1e671a09f29a25e218762c20a0e4
alaminproject2morrow <152832445+alaminproject2morrow@users.noreply.github.com>
2023-12-18 11:11:31 +0600

    init

---
+++ src/main/java/com/project2morrow/letsmeet/LetsmeetApplication.java
@@ -0,0 +1,13 @@
+package com.project2morrow.letsmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsmeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsmeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project2morrow/letsmeet/LetsmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2morrow.letsmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsmeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
